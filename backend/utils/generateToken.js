25cb8794ac7a731827ba6186e4ec3bddb95df6ea
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-04-04 02:40:42 +0530

    really last

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, //ms
     httpOnly: true, //prevent XSS attack, also known as cross-side scripting attacks
     sameSite: "strict", // CSRF attacks, cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+
   });
 };
 export default generateTokenAndSetCookie;
