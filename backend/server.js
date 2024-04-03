ad543e40d9ef05c059f549889a3608b2007c0a3d
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-04-04 02:06:31 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import dotenv from 'dotenv';
@@ -11,7 +12,7 @@ import { app, server } from './socket/socket.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 
-
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,6 +24,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 
 app.get("/", (req, res) => {
